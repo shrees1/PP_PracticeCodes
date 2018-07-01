7c184c1391c720a313520ab0ec51cf6b6dee500b
shrees1 <shreeshikha21@gmail.com>
2018-07-01 18:27:19 +0530

    Update Pangrams.java

--- Pangrams.java
+++ Pangrams.java
@@ -2,21 +2,20 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class Pangrams {
-
-	public static void main(String[] args) {
-		String panRef = "The quick brown fox jumps over the lazy dog";
-		String str=panRef.toLowerCase().replaceAll(" ", "");
-		char [] panChar=str.toCharArray();
-		Set panSet=new HashSet();
+	public boolean isPanagram(String panRef) {
+		boolean isString = panRef instanceof String;
+		if (!isString) {
+			return false;
+		}
+		String str = panRef.toLowerCase().replaceAll("[^a-zA-Z]+", "");
+		char[] panChar = str.toCharArray();
+		Set panSet = new HashSet();
 		for (char c : panChar) {
 			panSet.add(c);
 		}
-		  if(panSet.size() == 26)
-	           System.out.println("pangram");
-	        else
-	            System.out.println("not pangram");
+		if (panSet.size() == 26)
+			return true;
+		else
+			return false;
 	}
-
 }
-	
-
