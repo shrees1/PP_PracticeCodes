35f0029f82ea92439f57d7c7d333e4e8eb312b8b
shrees1 <shreeshikha21@gmail.com>
2018-07-01 18:28:11 +0530

    Create TestPangrams.java

---
+++ TestPangrams.java
@@ -0,0 +1,37 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestPangram {
+	Pangrams pangram = new Pangrams();
+
+	@Test
+	public void testValidCase() {
+		String panRef = "The quick brown fox jumps over the lazy dog";
+		Assert.assertTrue(pangram.isPanagram(panRef));
+	}
+
+	@Test
+	public void testFalseCase() {
+		String panRef = "Shikha Shree";
+		Assert.assertFalse(pangram.isPanagram(panRef));
+	}
+
+	@Test
+	public void testEmptyString() {
+		String panRef = "";
+		Assert.assertFalse(pangram.isPanagram(panRef));
+	}
+
+	@Test
+	public void testNull() {
+		String panRef = null;
+		Assert.assertFalse(pangram.isPanagram(panRef));
+	}
+
+	@Test
+	public void testSpecialCharacters() {
+		String panRef = "The quick ;p'[],./;;'';brown fox jumps over the lazy dog";
+		Assert.assertTrue(pangram.isPanagram(panRef));
+	}
+
+}
