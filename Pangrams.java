4bc7ca897f81e021226d3c6c71a48c8f98e4af0e
shrees1 <shreeshikha21@gmail.com>
2018-07-05 23:04:23 +0530

    Update Pangrams.java

--- Pangrams.java
+++ Pangrams.java
@@ -4,8 +4,7 @@ import java.util.regex.Pattern;
 
 /**
  * 
- * @author shshree
- * Testing whether a given string is a pangram
+ * @author shshree Testing whether a given string is a pangram
  */
 public class Pangrams {
 
@@ -17,33 +16,33 @@ public class Pangrams {
 	 */
 	public static boolean isPangram(String panRef) {
 
-		if(panRef==null) {
+		if (panRef == null) {
 			throw new NullPointerException("ERRORCODE: Value cannot be null");
 		}
 
-		if(panRef.length() < 26) {
-			throw new InputMismatchException("ERRORCODE: String doesn't qualify to be pangram so further checks won't happen");
+		if (panRef.length() < 26) {
+			throw new InputMismatchException(
+					"ERRORCODE: String doesn't qualify to be pangram so further checks won't happen");
 		}
 		String str = panRef.toLowerCase().replaceAll("\\s+", "");
 		Pattern regex = Pattern.compile("[$&+,:;=?@#|'<>.-^*()%!]");
-		//matcher to find if there is any special character in string
+		// matcher to find if there is any special character in string
 		Matcher matcher = regex.matcher(str);
 
-		if(matcher.find())
-		{
+		if (matcher.find()) {
 			throw new InputMismatchException("ERROCODE1: Invalid input value");
-		}
-		char[] panChar = str.toCharArray();
-		int[] albhabets = new int[26];
-		for(int i=0; i<panChar.length;i++) {
-			if(str.charAt(i)>=97 && str.charAt(i)<=122)
-				albhabets[str.charAt(i)-97]++;
-		}
-		if(albhabets.length==26) {
-			return true;
-		}
-		else {
-			return false;
+		} else {
+			char[] panChar = str.toCharArray();
+			int[] albhabets = new int[26];
+			for (int i = 0; i < panChar.length; i++) {
+				if (str.charAt(i) >= 97 && str.charAt(i) <= 122)
+					albhabets[str.charAt(i) - 97]++;
+			}
+			if (albhabets.length == 26) {
+				return true;
+			} else {
+				return false;
+			}
 		}
 	}
 }
